21112d18b22b42af5c97c3c7f02745fd61b70467
Eder Moros <edermorosrodero@gmail.com>
2025-03-26 08:59:53 +0100

    Feat: Creacion de Java Proyect, y creacion de clase Cliente

---
+++ SistemaTaller/src/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente {
+    // Atributos
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String direccion;
+    private String email;
+    private String dni;
+
+    // Constructor
+    public Cliente(String nombre, String apellido, String telefono, String direccion, String email, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.email = email;
+        this.dni = dni;
+    }
+}
