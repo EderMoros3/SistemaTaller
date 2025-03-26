5957025ad48a838a3a2aa6293942775e36467073
Eder Moros <edermorosrodero@gmail.com>
2025-03-26 09:35:17 +0100

    Feat: Creacion de menu

--- SistemaTaller/src/App.java
+++ SistemaTaller/src/App.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("Bienvenido al taller: ");
+            System.out.println("----------------------------");
+            System.out.println("1. Gestion de clientes");
+            System.out.println("2. Gestion de empleados");
+            System.out.println("3. Gestion de servicios");
+            System.out.println("4. Gestion de invetario: ");
+            System.out.println("5. Gestion de pedidos: ");
+            System.out.println("6. Gestion de vehiculos");
+            System.out.println("7. Gestion de citas");
+            System.out.println("8. Salir");
+            System.out.println("Selecciona una opcion: ");
+            opcion = sc.nextInt();
+
+        } while (opcion != 8);
+
+        switch (opcion) {
+            1 -> //gestionClientes();
+            2 -> //gestionEmpleados();
+            3 -> //gestionServicios();
+            4 -> //gestionInvetario();
+            5 -> //gestionPedidos();
+            6 -> //gestionVehiculos();
+            7 -> //gestionCitas();
+            8 -> System.out.println("El programa se cerro");
+        }
     }
 }
