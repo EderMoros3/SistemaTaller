7cf69b8fca6e76937d62ebee3d9f74e8fad3f8af
Eder Moros <edermorosrodero@gmail.com>
2025-03-26 10:00:42 +0100

    Feat: Metodo gestionClientes, agregarCliente y modificarCliente

--- SistemaTaller/src/Cliente.java
+++ SistemaTaller/src/Cliente.java
@@ -16,4 +16,20 @@ public class Cliente {
         this.email = email;
         this.dni = dni;
     }
+    
+    // Setters
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+    public void setTelefono(int telefono) { this.telefono = telefono; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public void setEmail(String email) { this.email = email; }
+    public void setDni(String dni) { this.dni = dni; }
+
+    // Getters
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public int getTelefono() { return telefono; }
+    public String getDireccion() { return direccion; }
+    public String getEmail() { return email; }
+    public String getDni() { return dni; }
 }
---
+++ SistemaTaller/src/Taller.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Taller {
+    // Atributos
+    ArrayList<Cliente> listaClientes = new ArrayList<>();
+
+    Scanner sc = new Scanner(System.in);
+    public void gestionClientes() {
+        int opcion;
+        do {
+            System.out.println("Gestion de clientes");
+        System.out.println("1. Agregar cliente");
+        System.out.println("2. Modificar cliente");
+        System.out.println("3. Eliminar cliente");
+        System.out.println("4. Listar clientes");
+        System.out.println("5. Volver al menu principal");
+        System.out.println("Ingrese una opcion: ");
+        opcion = sc.nextInt();
+        } while (opcion != 5);
+        
+        switch (opcion) {
+            case 1 -> agregarCliente();
+            case 2 -> modificarCliente();
+            case 3 -> //eliminarCliente();
+            case 4 -> //listarClientes();
+            case 5 -> System.out.println("Volviendo al menu principal");
+        }
+    }
+
+    public void agregarCliente() {
+        System.out.println("Agregar cliente");
+        System.out.println("Introduce el nombre: ");
+        String nombre = sc.nextLine();
+        sc.next();
+        System.out.println("Introduce el apellido: ");
+        String apellido = sc.nextLine();
+        sc.next();
+        System.out.println("Introduce el telefono: ");
+        int telefono = sc.nextInt();
+        System.out.println("Introduce la direccion: ");
+        String direccion = sc.nextLine();
+        sc.next();
+        System.out.println("Introduce el email: ");
+        String email = sc.nextLine();
+        sc.next();
+        System.out.println("Introduce el dni: ");
+        String dni = sc.nextLine();
+        sc.next();
+
+        Cliente cliente = new Cliente(nombre, apellido, telefono, direccion, email, dni);
+        listaClientes.add(cliente);
+        System.out.println("Cliente agregado correctamente");
+    }
+
+    public Cliente getClienteDni() {
+        String dni;
+        if (!this.listaClientes.isEmpty()) {
+            do {
+                System.out.println("Introduce el dni del cliente: ");
+                dni = sc.nextLine();
+                sc.next();
+                for (Cliente cliente : this.listaClientes) {
+                    if (cliente.getDni().equals(dni)) {
+                        return cliente;
+                    }
+                }
+                System.out.println("No se encontro ningun cliente con ese dni");
+            } while (true);
+        }
+        return null;
+    }   
+
+    public void modificarCliente() {
+        Cliente cliente = this.getClienteDni();
+        int opcion;
+        do {
+            System.out.println("Modificar cliente");
+            System.out.println("1. Modificar nombre");
+            System.out.println("2. Modificar apellido");
+            System.out.println("3. Modificar telefono");
+            System.out.println("4. Modificar direccion");
+            System.out.println("5. Modificar email");
+            System.out.println("6. Modificar dni");
+            System.out.println("7. Volver al menu anterior");
+            System.out.println("Ingrese una opcion: ");
+            opcion = sc.nextInt();
+        } while (opcion != 7);
+
+        switch (opcion) {
+            case 1 -> {
+                System.out.println("Introduce el nombre: ");
+                String nombre = sc.nextLine();
+                sc.next();
+                cliente.setNombre(nombre);
+                System.out.println("Nombre modificado correctamente");
+            }
+            case 2 -> {
+                System.out.println("Introduce el apellido: ");
+                String apellido = sc.nextLine();
+                sc.next();
+                cliente.setApellido(apellido);
+                System.out.println("Apellido modificado correctamente");
+            }
+            case 3 -> {
+                System.out.println("Introduce el telefono: ");
+                int telefono = sc.nextInt();
+                cliente.setTelefono(telefono);
+                System.out.println("Telefono modificado correctamente");
+            }
+            case 4 -> {
+                System.out.println("Introduce la direccion: ");
+                String direccion = sc.nextLine();
+                sc.next();
+                cliente.setDireccion(direccion);
+                System.out.println("Direccion modificada correctamente");
+            }
+            case 5 -> {
+                System.out.println("Introduce el email: ");
+                String email = sc.nextLine();
+                sc.next();
+                cliente.setEmail(email);
+                System.out.println("Email modificado correctamente");
+            }
+            case 6 -> {
+                System.out.println("Introduce el dni: ");
+                String dni = sc.nextLine();
+                sc.next();
+                cliente.setDni(dni);
+                System.out.println("Dni modificado correctamente");
+            }
+            case 7 -> System.out.println("Volviendo al menu anterior");
+        }
+        
+    }
+}
