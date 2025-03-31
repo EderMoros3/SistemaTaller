11fcb3ae2c8e5ed71c99f4862f99e1447f5ea42c
Nicolas Blasco <nicolaslol22@gmail.com>
2025-03-31 12:21:40 +0200

    creacion consultas empleado, fix consulta tlf>apellido en actualizarTlfCliente

--- SistemaTaller/src/dao/EmpleadoDAO.java
+++ SistemaTaller/src/dao/EmpleadoDAO.java
@@ -1,41 +1,166 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-
 import model.Empleado;
 
 public class EmpleadoDAO {
     public void insertarEmpleado(Empleado empleado) {
-        // INSERT SQL
+        String nombre = empleado.getNombre();
+        String apellido = empleado.getApellido();
+        int idEmpleado = empleado.getIdEmpleado();
+        int telefono = empleado.getTelefono();
+
+        Connection conexion = ConexionDB.conectar();
+        
+        if (conexion != null) {
+            String query = "INSERT INTO Empleado (nombre, apellido, idEmpleado, telefono) VALUES ( ?, ?, ? ,?)";
+            
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setString(1, nombre);
+                ps.setString(2, apellido);
+                ps.setInt(3, idEmpleado);
+                ps.setInt(4, telefono);
+                ps.executeUpdate();
+                
+            } catch (SQLException e) {
+                System.err.println("Error al insertar empleado: " + e.getMessage());
+            }
+        
+        
+        
+        }
     }
 
     public void eliminarEmpleado(Empleado empleado) {
-        // DELETE SQL
+        int idEmpleado = empleado.getIdEmpleado();
+
+        Connection conexion = ConexionDB.conectar();
+
+        if (conexion != null) {
+            String query = "DELETE FROM Empledao WHERE idEmpleado = ?";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setInt(1, idEmpleado);
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al eliminar empleado: " + e.getMessage());
+                
+            }
+        }
     }
 
-    public Empleado getEmpleadoID() {
-        // SELECT SQL
-        return null;
+    public Empleado getIdEmpleado(int idEmpleado) {
+        Connection conexion = ConexionDB.conectar();
+        Empleado empleado = null;
+
+        if (conexion != null) {
+            String query = "SELECT * FROM Empleado WHERE idEmpleado = ?";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setInt(1, idEmpleado);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String nombre = rs.getString("nombre");
+                        String apellido = rs.getString("apellido");
+                        int telefono = rs.getInt("telefono");
+
+                        empleado = new Empleado(nombre, apellido, telefono, idEmpleado);
+                    }
+                }
+            } catch (SQLException e) {
+                System.err.println("Error al obtener empleado por ID: " + e.getMessage());
+            }
+        }
+        return empleado;
     }
 
     public ArrayList<Empleado> listarEmpleados() {
-        // SELECT SQL
+        Connection conexion = ConexionDB.conectar();
+        ArrayList<Empleado> empleados = new ArrayList<>();
+
+        if (conexion != null) {
+            String query = "SELECT * FROM Empleado";
+
+            try (Statement stmt = conexion.createStatement();
+            ResultSet rs = stmt.executeQuery(query)) {
+                while (rs.next()) {
+                    String nombre = rs.getString("nombre");
+                    String apellido = rs.getString("apellido");
+                    int idEmpleado = rs.getInt("idEmpleado");
+                    int telefono = rs.getInt("telefono");
+                    
+
+                    Empleado empleado = new Empleado(nombre, apellido, idEmpleado, telefono);
+                    empleados.add(empleado);
+                }
+            } catch (SQLException e) {
+                System.err.println("Error al listar empleados: " + e.getMessage());
+            }
+        return empleados;
+        }
         return null;
     }
 
-    public void modificarNombreEmpleado(String nombre) {
-        // UPDATE SQL
+    public void actualizarNombreEmpleado(String nombre, int idEmpleado) {
+        Connection conexion = ConexionDB.conectar();
+
+        if (conexion != null) {
+            String query = "UPDATE Empleado SET nombre = ? WHERE idEmpleado = ?";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setString(1, nombre);
+                ps.setInt(2, idEmpleado);
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al actualizar nombre del empleado: " + e.getMessage());
+            }
+        }
     }
 
-    public void modificarApellidoEmpleado(String apellido) {
-        // UPDATE SQL
+    public void actualizarApellidoEmpleado(String apellido, int idEmpleado) {
+        Connection conexion = ConexionDB.conectar();
+        if (conexion != null) {
+            String query = "UPDATE Empleado SET apellido = ? WHERE idEmpleado = ?";
+            try(PreparedStatement stmt = conexion.prepareStatement(query)){
+                stmt.setString(1, apellido);
+                stmt.setInt(2, idEmpleado);
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al actualizar apellido del empleado: " + e.getMessage());
+            }
+        }
     }
 
-    public void modificarTlfEmpleado(int telefono) {
-        // UPDATE SQL
+    public void actualizarTlfEmpleado(int telefono, int idEmpleado) {
+        Connection conexion = ConexionDB.conectar();
+        if (conexion != null) {
+            String query = "UPDATE Empleado SET telefono = ? WHERE dni = ?";
+            try(PreparedStatement stmt = conexion.prepareStatement(query)){
+                stmt.setInt(1, telefono);
+                stmt.setInt(2, idEmpleado);
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al actualizar telefono del empleado: " + e.getMessage());
+            }
+        }
     }
 
-    public void modificiarIdEmpleado(int id) {
-        // UPDATE SQL
+    public void actualizarIdEmpleado(int idEmpleado) {
+        Connection conexion = ConexionDB.conectar();
+        if (conexion != null) {
+            String query = "UPDATE Cliente SET idEmpleado = ? WHERE idEmpleado = ?";
+            try(PreparedStatement stmt = conexion.prepareStatement(query)){
+                stmt.setInt(1, idEmpleado);
+                stmt.setInt(2, idEmpleado);
+                stmt.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al actualizar ID del empleado: " + e.getMessage());
+            }
+        }
     }
 }
--- SistemaTaller/src/dao/VehiculoDAO.java
+++ SistemaTaller/src/dao/VehiculoDAO.java
@@ -1,12 +1,40 @@
 package dao;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import model.Cliente;
 
 import model.Vehiculo;
 
 public class VehiculoDAO {
     public void insertarVehiculo(Vehiculo vehiculo) {
-        // INSERT SQL
+        String matricula = vehiculo.getMatricula();
+        String marca = vehiculo.getMarca();
+        String modelo = vehiculo.getModelo();
+        int year = vehiculo.getYear();
+        String dni = cliente.getDni();
+        
+        Connection conexion = ConexionDB.conectar();
+
+        if (conexion != null) {
+            String query = "INSERT INTO Vehiculo (matricula, marca, modelo, year) VALUES (?, ?, ?, ?)";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setString(1, matricula);
+                ps.setString(2, marca);
+                ps.setString(3, modelo);
+                ps.setInt(4, year);
+                ps.executeUpdate();
+
+            } catch (SQLException e) {
+                System.err.println("Error al insertar vehiculo: " + e.getMessage());
+            }
+        }
+
     }
 
     public void eliminarVehiculo(Vehiculo vehiculo) {
