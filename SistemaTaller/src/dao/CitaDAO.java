4366f68d8ba8292d5ace7a551a5a6ef0a0818b5e
Eder Moros <edermorosrodero@gmail.com>
2025-04-01 10:36:01 +0200

    Feat: CitaDAO terminado y modifiaciones en CitaView

--- SistemaTaller/src/dao/CitaDAO.java
+++ SistemaTaller/src/dao/CitaDAO.java
@@ -1,41 +1,169 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import dao.ClienteDAO;
 import model.Cita;
+import model.Cliente;
+import view.ClienteView;
 
 public class CitaDAO {
+    ClienteDAO clienteDAO = new ClienteDAO();
     public void insertarCita(Cita cita) {
-        // INSERT SQL
+        String fecha = cita.getFecha();
+        String hora = cita.getHora();
+        String descripcion = cita.getDescripcion();
+        Cliente cliente = cita.getCliente();
+
+        Connection conexion = ConexionDB.conectar();
+        if (conexion != null) {
+            String query = "INSERT INTO Cita (fecha, hora, descripcion, cliente) VALUES ( ?, ?, ? ,?)";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setString(1, fecha);
+                ps.setString(2, hora);
+                ps.setString(3, descripcion);
+                ps.setString(4, cliente.getDni());
+                ps.executeUpdate();
+
+            } catch (SQLException e) {
+                System.err.println("Error al insertar cita: " + e.getMessage());
+            }
+        }
     }
 
     public void eliminarCita(Cita cita) {
-        // DELETE SQL
+        int idCita = cita.getIdCita();
+        Connection conexion = ConexionDB.conectar();
+
+        if (conexion != null) {
+            String query = "DELETE FROM Cita WHERE idCita = ?";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setInt(1, idCita);
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al eliminar cita: " + e.getMessage());
+            }
+        }
     }
 
     public ArrayList<Cita> listarCitas() {
-        // SELECT SQL
+        Connection conexion = ConexionDB.conectar();
+        ArrayList<Cita> citas = new ArrayList<>();
+
+        if (conexion != null) {
+            String query = "SELECT * FROM Cita";
+            try (Statement stmt = conexion.createStatement();
+                ResultSet rs = stmt.executeQuery(query)) {
+                int idCita = rs.getInt("idCita");
+                String fecha = rs.getString("fecha");
+                String hora = rs.getString("hora");
+                String descripcion = rs.getString("descripcion");
+                Cliente cliente = clienteDAO.getClienteDni(rs.getString("dni"));
+                citas.add(new Cita(idCita, cliente, fecha, hora, descripcion ));
+            } catch (SQLException e) {
+                System.err.println("Error al listar citas: " + e.getMessage());
+            }
+        }
         return null;
     }
 
-    public Cita getCitaId() {
-        // SELECT SQL
+    public Cita getCitaId(int idCita) {
+        Connection conexion = ConexionDB.conectar();
+        Cita cita = null;
+
+        if (conexion != null) {
+            String query = "SELECT * FROM Cita WHERE idCita = ?";
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setInt(1, idCita);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        String fecha = rs.getString("fecha");
+                        String hora = rs.getString("hora");
+                        String descripcion = rs.getString("descripcion");
+                        Cliente cliente = clienteDAO.getClienteDni(rs.getString("dni"));
+                        cita = new Cita(idCita, cliente, fecha, hora, descripcion);
+                    }
+                }
+            } catch (SQLException e) {
+                System.err.println("Error al obtener cita: " + e.getMessage());
+            }
+        }
         return null;
     }
 
-    public void modificarClienteCita(String dni) {
-        // UPDATE SQL
+    public void modificarClienteCita(Cliente cliente, Cita cita) {
+        String dni = cliente.getDni();
+        int idCita = cita.getIdCita();
+        Connection conexion = ConexionDB.conectar();
+
+        if (conexion != null) {
+            String query = "UPDATE Cita SET dni = ? WHERE idCita = ?";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setString(1, dni);
+                ps.setInt(2, idCita);
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al modificar cita: " + e.getMessage());
+            }
+        }
     }
 
-    public void modificarFechaCita(String fecha) {
-        // UPDATE SQL
+    public void modificarFechaCita(Cita cita, String fecha) {
+        Connection conexion = ConexionDB.conectar();
+
+        int idCita = cita.getIdCita();
+        if (conexion != null) {
+            String query = "UPDATE Cita SET fecha = ? WHERE idCita = ?";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setString(1, fecha);
+                ps.setInt(2, idCita);
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al modificar cita: " + e.getMessage());
+            }
+        }
     }
 
-    public void modificarHoraCita(String hora) {
-        // UPDATE SQL
+    public void modificarHoraCita(Cita cita, String hora) {
+        Connection conexion = ConexionDB.conectar();
+
+        int idCita = cita.getIdCita();
+        if (conexion != null) {
+            String query = "UPDATE Cita SET hora = ? WHERE idCita = ?";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setString(1, hora);
+                ps.setInt(2, idCita);
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al modificar cita: " + e.getMessage());
+            }
+        }
     }
 
-    public void modificarDescripcionCita(String descripcion) {
-        // UPDATE SQL
+    public void modificarDescripcionCita(Cita cita, String descripcion) {
+        Connection conexion = ConexionDB.conectar();
+
+        int idCita = cita.getIdCita();
+        if (conexion != null) {
+            String query = "UPDATE Cita SET descripcion = ? WHERE idCita = ?";
+
+            try (PreparedStatement ps = conexion.prepareStatement(query)) {
+                ps.setString(1, descripcion);
+                ps.setInt(2, idCita);
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error al modificar cita: " + e.getMessage());
+            }
+        }
     }
 }
